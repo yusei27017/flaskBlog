14c563b891045f87351aed9716208745568ccdea
yusei27017 <no101shark@gmail.com>
2021-12-01 12:25:29 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# flaskBlog
+# flask框架搭建個人Blog
+
+## 安裝nodejs npm
+```shell
+sudo apt install nodejs
+sudo apt install npm
+```
+
+## 安裝PM2
+```shell
+npm install pm2 -g
+pm2 completion install
+```
+
+## 安裝nginx
+```shell
+sudo apt install nginx
+```
+
+## gcp setting 
+查看網路資料>防火牆>建立防火牆規則>
+設定：方向（輸入）範圍：(0.0.0.0/0)
+通訊協定和通訊埠：（tpc: xxxx）
+
+## 更改DNS託管
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template, redirect
+from flask_pymongo import PyMongo
 
 app = app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/flaskBlog"
+mongo = PyMongo(app)
 
 @app.route("/")
 def homePage():
@@ -19,3 +21,38 @@ def aboutMe():
 @app.route("/aboutSite")
 def aboutSite():
     return render_template("aboutSite.html")
+
+@app.route("/pixivPage")
+def pixivPage():
+    return render_template("pixivPage.html")
+
+@app.route("/ps5Page")
+def ps5Page():
+    return render_template("ps5Page.html")
+
+@app.route("/skillPage")
+def skillPage():
+    return render_template("skillPage.html")
+
+@app.route("/aboutMeData")
+def aboutMeData():
+    res = mongo.db.aboutMe.find({'sort':'aboutMe'})
+    for v in res:
+        data = v['data']
+    return jsonify(data=data)
+
+@app.route("/projectData")
+def projectData():
+    res = mongo.db.aboutMe.find({'sort':'projectInfo'})
+    for v in res:
+        data = v['data']
+    return jsonify(data=data)
+
+@app.route("/mongodb")
+def mongodb():
+    test = mongo.db.aboutMe.find({})
+    # print(test.execute())
+    for i in test:
+        print(i['stadyexp'])
+    print('test')
+    return f"200 ok: {i}"
